c58ae38d316294c0653d8dfcc300c90e6978e82b
Alexander Wang <zxcvb22217@gmail.com>
2020-11-11 22:37:54 +0800

    feat: Make Baby only show btns when it's a Block

--- src/content/Baby.tsx
+++ src/content/Baby.tsx
@@ -7,25 +7,33 @@ export class Baby extends React.Component<ContentProps<unknown>> {
   }
 
   render(): JSX.Element {
-    return (
-      <>
-        <style jsx>{`
-          div {
-            padding: 0.3rem 1.5rem;
-          }
+    switch (this.props.viewMode) {
+      case 'block': {
+        return (
+          <>
+            <style jsx>{`
+              div {
+                padding: 0.3rem 1.5rem;
+              }
+    
+              p {
+                margin: 0;
+              }
+            `}</style>
+            <div>
+              <p>Which to turn into?</p>
+              <button onClick={() => { this.props.onReplace('text') }}>Text</button>
+              <button onClick={() => { this.props.onReplace('pmtext') }}>PMText</button>
+              <button onClick={() => { this.props.onReplace('image') }}>Image</button>
+              <button onClick={() => { this.props.onReplace('status') }}>Status</button>
+            </div>
+          </>
+        )
+      }
+      default: {
+        return <span>Baby</span>
+      }
+    }
 
-          p {
-            margin: 0;
-          }
-        `}</style>
-        <div>
-          <p>Which to turn into?</p>
-          <button onClick={() => { this.props.onReplace('text') }}>Text</button>
-          <button onClick={() => { this.props.onReplace('pmtext') }}>PMText</button>
-          <button onClick={() => { this.props.onReplace('image') }}>Image</button>
-          <button onClick={() => { this.props.onReplace('status') }}>Status</button>
-        </div>
-      </>
-    )
   }
 }
