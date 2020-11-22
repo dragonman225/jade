670f90f6af2178a53dd9fb160a588df6ccef27df
Alexander Wang <zxcvb22217@gmail.com>
2020-11-22 23:15:24 +0800

    refactor: Rewrite Image Block with hooks

--- src/content/Image.tsx
+++ src/content/Image.tsx
@@ -1,117 +1,141 @@
 import * as React from 'react'
 import { ContentProps } from '../interfaces'
 
-interface ImageContent {
-  loaded: boolean
-  imgSrc: string
+interface Content {
+  valid: boolean
+  imgData: string
 }
 
-interface State {
-  loaded: boolean
-  imgSrc: string
+function readAsDataUrl(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.addEventListener('load', (event) => {
+      const img = event.target.result.toString() // a data-url
+      resolve(img)
+    })
+    reader.addEventListener('error', (e) => {
+      reject(e)
+    })
+    reader.readAsDataURL(file)
+  })
 }
 
-/**
- * BUG: When props change after instantiated, state doesn't update.
- */
+const ImgState = {
+  NotLoaded: Symbol('notloaded'),
+  Loaded: Symbol('loaded'),
+  Error: Symbol('error')
+}
 
-export class Image extends React.Component<ContentProps<unknown>, State> {
-  constructor(props: ContentProps<unknown>) {
-    super(props)
-    if (props.content === null) {
-      this.state = { loaded: false, imgSrc: '' }
-    } else {
-      this.state = props.content as ImageContent
-    }
-  }
+export const Image: React.FunctionComponent<ContentProps<unknown>> = (props) => {
+  const content = props.content as Content
+  const [imgState, setImgState] = React.useState(ImgState.NotLoaded)
+  const [img, setImg] = React.useState('')
+  const [error, setError] = React.useState('')
 
-  handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const file = e.target.files[0]
     console.log(file)
     if (file.type && file.type.indexOf('image') === -1) {
       console.log('File is not an image.', file.type, file)
       return
     }
-    const reader = new FileReader()
-    reader.addEventListener('load', (event) => {
-      const img = event.target.result.toString() // a data-url
-      const newContent = {
-        loaded: true,
-        imgSrc: img
-      }
-      this.props.onChange(newContent)
-      this.setState(newContent)
-    })
-    reader.readAsDataURL(file)
+    readAsDataUrl(file)
+      .then((img) => {
+        setImg(img)
+        setImgState(ImgState.Loaded)
+        props.onChange({
+          valid: true,
+          imgData: img
+        })
+      })
+      .catch((err) => {
+        setError(err)
+        setImgState(ImgState.Error)
+      })
   }
 
-  render(): JSX.Element {
-    switch (this.props.viewMode) {
-      case 'nav_item':
-        return (
-          <>
-            <style jsx>{`
-              div {
-                height: 100%;
-                max-height: inherit;
-                overflow: hidden;
-              }
-              img {
-                width: 100%;
-                height: 100%;
-                max-height: inherit;
-                object-fit: contain;
-              }
-            `}</style>
-            {
-              this.state.loaded
-                ? <div><img src={this.state.imgSrc} draggable={false} /></div>
-                : <span>Image not loaded.</span>
-            }
-          </>
-        )
-      default:
-        return (
-          <>
-            <style jsx>{`
-              .ImgViewer {
-                height: 100%;
-                overflow: hidden;
-              }
-              
-              .ImgChooser {
-                padding: 0.3rem 1.5rem;
-              }
-
-              img {
-                width: 100%;
-              }
-
-              img[data-view-mode='card'] {
-                height: 100%;
-                object-fit: contain;
-                object-position: left center;
-              }
-            `}</style>
-            {
-              this.state.loaded
-                ? (
-                  <div className="ImgViewer">
-                    <img
-                      src={this.state.imgSrc}
-                      draggable={false}
-                      data-view-mode={this.props.viewMode} />
-                  </div>
-                )
-                : (
-                  <div className="ImgChooser">
-                    <input type="file" accept=".jpg, .jpeg, .png"
-                      onChange={this.handleFileSelect} />
-                  </div>
-                )
-            }
-          </>
-        )
+  /** Load image on first render or props change. */
+  React.useEffect(() => {
+    if (content && content.valid) {
+      setImg(content.imgData)
+      setImgState(ImgState.Loaded)
     }
+  }, [props.content])
+
+  switch (props.viewMode) {
+    case 'nav_item':
+      return (
+        <>
+          <style jsx>{`
+            div {
+              height: 100%;
+              max-height: inherit;
+              overflow: hidden;
+            }
+
+            img {
+              width: 100%;
+              height: 100%;
+              max-height: inherit;
+              object-fit: contain;
+            }
+
+            span {
+              font-size: 0.8rem;
+              padding: 0.5rem;
+              max-height: 100%;
+            }
+          `}</style>
+          {
+            imgState === ImgState.Loaded
+              ? <div><img src={img} draggable={false} /></div>
+              : <span>Image not loaded or error occurred.</span>
+          }
+        </>
+      )
+    default:
+      return (
+        <>
+          <style jsx>{`
+            .ImgViewer {
+              height: 100%;
+              overflow: hidden;
+            }
+            
+            .ImgChooser {
+              padding: 0.3rem 1.5rem;
+            }
+
+            .ImgChooser > input {
+              width: 250px;
+            }
+
+            img {
+              width: 100%;
+            }
+
+            img[data-view-mode='card'] {
+              height: 100%;
+              object-fit: contain;
+              object-position: left center;
+            }
+          `}</style>
+          {
+            imgState === ImgState.Loaded ? (
+              <div className="ImgViewer">
+                <img
+                  src={img}
+                  draggable={false}
+                  data-view-mode={props.viewMode} />
+              </div>
+            ) : imgState === ImgState.NotLoaded ? (
+              <div className="ImgChooser">
+                <input type="file" accept=".jpg, .jpeg, .png"
+                  onChange={handleFileSelect} />
+              </div>
+            ) : <span>{error}</span>
+          }
+        </>
+      )
   }
 }
