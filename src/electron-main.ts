54ae067882876749cf7a61b4041e157ba2e34284
Alexander Wang <zxcvb22217@gmail.com>
2020-12-30 03:10:17 +0800

    feat: Make default window size larger

--- src/electron-main.ts
+++ src/electron-main.ts
@@ -4,8 +4,8 @@ import { app, BrowserWindow } from 'electron'
 function createWindow() {
   console.log(`Jade runs from dir "${__dirname}", and the entry script is "${__filename}".`)
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     icon: path.resolve(__dirname, '../512x512.png'),
     webPreferences: {
       nodeIntegration: true
