dac59cad1ef1fb2506e08453ddb6b952f47d829e
Alexander Wang <zxcvb22217@gmail.com>
2021-05-23 15:27:51 +0800

    refactor(useAnimationFrame): Clean up code

--- src/core/useAnimationFrame.ts
+++ src/core/useAnimationFrame.ts
@@ -1,12 +1,10 @@
-import * as React from 'react'
+import { useRef, useEffect } from 'react'
 
-export const useAnimationFrame = (
-  callback: (deltaTime: number) => void
-): void => {
+export function useAnimationFrame(callback: (deltaTime: number) => void): void {
   // Use useRef for mutable variables that we want to persist
   // without triggering a re-render on their change
-  const requestRef = React.useRef<number>()
-  const previousTimeRef = React.useRef<number>()
+  const requestRef = useRef<number>()
+  const previousTimeRef = useRef<number>()
 
   const animate = (time: number) => {
     if (previousTimeRef.current != undefined) {
@@ -17,7 +15,7 @@ export const useAnimationFrame = (
     requestRef.current = requestAnimationFrame(animate)
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
     requestRef.current = requestAnimationFrame(animate)
     return () => cancelAnimationFrame(requestRef.current)
   }, []) // Make sure the effect runs only once
