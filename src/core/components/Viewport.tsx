0cbface3ea64f55ed8260a81ae71eebd2cd1235a
Alexander Wang <zxcvb22217@gmail.com>
2021-08-19 21:41:39 +0800

    fix(Viewport): Detect pointerup not from Viewport
    * So that panning and selecting stop normally when pointerup at
      non-Viewport areas.

--- src/core/components/Viewport.tsx
+++ src/core/components/Viewport.tsx
@@ -64,6 +64,43 @@ export function Viewport(props: Props): JSX.Element {
       let selecting = false
       let lastClientCoords = { x: 0, y: 0 }
 
+      function handlePointerMove(e: MouseEvent | TouchEvent) {
+        const clientCoords = getUnifiedClientCoords(e)
+
+        if (panning) {
+          const movement = vecSub(clientCoords, lastClientCoords)
+          dispatchAction({
+            type: Action.CameraMoveDelta,
+            data: movement,
+          })
+        } else if (selecting) {
+          dispatchAction({
+            type: Action.SelectionBoxSetEnd,
+            data: clientCoords,
+          })
+        }
+
+        lastClientCoords = clientCoords
+      }
+
+      function handlePointerUp() {
+        panning = false
+        if (selecting) {
+          dispatchAction({
+            type: Action.SelectionBoxClear,
+          })
+          selecting = false
+        }
+
+        lastClientCoords = { x: 0, y: 0 }
+
+        window.removeEventListener('mousemove', handlePointerMove)
+        window.removeEventListener('mouseup', handlePointerUp)
+
+        window.removeEventListener('touchmove', handlePointerMove)
+        window.removeEventListener('touchend', handlePointerUp)
+      }
+
       return {
         handlePointerDown: (e: MouseEvent | TouchEvent) => {
           const clientCoords = getUnifiedClientCoords(e)
@@ -94,35 +131,12 @@ export function Viewport(props: Props): JSX.Element {
           }
 
           lastClientCoords = clientCoords
-        },
-        handlePointerMove: (e: MouseEvent | TouchEvent) => {
-          const clientCoords = getUnifiedClientCoords(e)
 
-          if (panning) {
-            const movement = vecSub(clientCoords, lastClientCoords)
-            dispatchAction({
-              type: Action.CameraMoveDelta,
-              data: movement,
-            })
-          } else if (selecting) {
-            dispatchAction({
-              type: Action.SelectionBoxSetEnd,
-              data: clientCoords,
-            })
-          }
+          window.addEventListener('mousemove', handlePointerMove)
+          window.addEventListener('mouseup', handlePointerUp)
 
-          lastClientCoords = clientCoords
-        },
-        handlePointerUp: () => {
-          panning = false
-          if (selecting) {
-            dispatchAction({
-              type: Action.SelectionBoxClear,
-            })
-            selecting = false
-          }
-
-          lastClientCoords = { x: 0, y: 0 }
+          window.addEventListener('touchmove', handlePointerMove)
+          window.addEventListener('touchend', handlePointerUp)
         },
         handleKeydown: (e: KeyboardEvent) => {
           if (e.altKey && e.key === 'n') {
@@ -145,25 +159,17 @@ export function Viewport(props: Props): JSX.Element {
     cameraEl.addEventListener('wheel', handleWheel)
     cameraEl.addEventListener('dblclick', handleDoubleClick)
     cameraEl.addEventListener('mousedown', inputDetector.handlePointerDown)
-    window.addEventListener('mousemove', inputDetector.handlePointerMove)
-    cameraEl.addEventListener('mouseup', inputDetector.handlePointerUp)
     cameraEl.addEventListener('touchstart', inputDetector.handlePointerDown)
-    window.addEventListener('touchmove', inputDetector.handlePointerMove)
-    cameraEl.addEventListener('touchend', inputDetector.handlePointerUp)
     window.addEventListener('keydown', inputDetector.handleKeydown)
 
     return () => {
       cameraEl.removeEventListener('wheel', handleWheel)
       cameraEl.removeEventListener('dblclick', handleDoubleClick)
       cameraEl.removeEventListener('mousedown', inputDetector.handlePointerDown)
-      window.removeEventListener('mousemove', inputDetector.handlePointerMove)
-      cameraEl.removeEventListener('mouseup', inputDetector.handlePointerUp)
       cameraEl.removeEventListener(
         'touchstart',
         inputDetector.handlePointerDown
       )
-      window.removeEventListener('touchmove', inputDetector.handlePointerMove)
-      cameraEl.removeEventListener('touchend', inputDetector.handlePointerUp)
       window.removeEventListener('keydown', inputDetector.handleKeydown)
     }
   }, [dispatchAction])
