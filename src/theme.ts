a87dba69cc3285548e46f7004f42f3630b894b2e
Alexander Wang <zxcvb22217@gmail.com>
2021-08-30 01:08:40 +0800

    feat(theme): Change primary colors and add paddings

--- src/theme.ts
+++ src/theme.ts
@@ -7,12 +7,21 @@ export default {
     bgGrey: '#eeeeee',
     bgHover: 'rgba(55, 53, 47, 0.08)',
     bgActive: 'rgba(55, 53, 47, 0.16)',
-    uiPrimary: 'rgb(115, 0, 216)',
-    uiPrimaryLight: 'rgba(115, 0, 216, 0.8)',
-    uiPrimaryVeryLight: 'rgba(115, 0, 216, 0.2)',
+    /** First-gen purple-based color */
+    // uiPrimary: 'rgb(115, 0, 216)',
+    // uiPrimaryLight: 'rgba(115, 0, 216, 0.8)',
+    // uiPrimaryVeryLight: 'rgba(115, 0, 216, 0.2)',
+    /** Second-gen green-based color */
+    uiPrimary: 'hsl(165deg, 100%, 33%)',
+    uiPrimaryLight: 'hsla(165deg, 100%, 33%, 0.8)',
+    uiPrimaryVeryLight: 'hsla(165deg, 100%, 33%, 0.2)',
+    primary2: 'hsl(165deg, 100%, 33%)',
+    primary2Hover: 'hsl(165deg, 100%, 39%)',
+    primary2Active: 'hsl(165deg, 100%, 35%)',
     uiGrey: '#8e8e8e',
     uiGreyLight: '#e4e4e4',
     uiBlack: 'rgb(55, 53, 47)',
+    uiWhite: '#fff',
   },
   shadows: {
     ui: `
@@ -33,5 +42,11 @@ export default {
   easings: {
     /** @see https://easings.net/#easeInOutCubic */
     easeInOutCubic: 'cubic-bezier(0.65, 0, 0.35, 1)',
+  },
+  paddings: {
+    /** Create comfortable paddings for view mode "block". */
+    blockComfort: '.5rem 1.25rem',
+    /** Create comfortable paddings for view mode "navItem". */
+    navComfort: '.5rem',
   },
 }
