331e8065ddcdb5045f3a3381ee63102b5117c9e0
Alexander Wang <zxcvb22217@gmail.com>
2021-08-30 01:09:15 +0800

    feat(lightComponents): Add reusable styles for button and input

---
+++ src/lightComponents.ts
@@ -0,0 +1,49 @@
+import theme from './theme'
+
+export const buttonBase = {
+  outline: 'none',
+  border: 'none',
+  borderRadius: theme.borders.smallRadius,
+  padding: '.3rem .5rem',
+  fontSize: '.875rem',
+  lineHeight: '1.25rem',
+  transition: 'background 0.1s ease-in-out',
+  cursor: 'pointer',
+}
+
+export const buttonTransparent = {
+  ...buttonBase,
+  color: theme.colors.uiBlack,
+  background: theme.colors.bgWhite,
+  $nest: {
+    '&:hover': {
+      background: theme.colors.bgHover,
+    },
+    '&:active': {
+      background: theme.colors.bgActive,
+    },
+  },
+}
+
+export const buttonPrimary = {
+  ...buttonBase,
+  color: theme.colors.uiWhite,
+  background: theme.colors.primary2,
+  $nest: {
+    '&:hover': {
+      background: theme.colors.primary2Hover,
+    },
+    '&:active': {
+      background: theme.colors.primary2Active,
+    },
+  },
+}
+
+export const input = {
+  outline: 'none',
+  border: 'none',
+  borderRadius: theme.borders.smallRadius,
+  padding: '.3rem .5rem',
+  background: theme.colors.bgGrey,
+  lineHeight: 1.25,
+}
