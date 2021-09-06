af031e61a9d80671b2c1f55663735fee350cebd8
Alexander Wang <zxcvb22217@gmail.com>
2021-09-06 16:31:10 +0800

    feat(Icons): Switch to round ones

--- src/core/components/Icons/ExpandDown.tsx
+++ src/core/components/Icons/ExpandDown.tsx
@@ -16,8 +16,8 @@ export const ExpandDown: React.FunctionComponent = () => {
       height="100%"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg">
-      <path d="M0 0h24v24H0z" fill="none" />
-      <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
+      <path d="M24 24H0V0h24v24z" fill="none" opacity=".87" />
+      <path d="M15.88 9.29L12 13.17 8.12 9.29c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41-.39-.38-1.03-.39-1.42 0z" />
     </svg>
   )
 }
