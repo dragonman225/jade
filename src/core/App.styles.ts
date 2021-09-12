2721774eec946775f4d778c429b7ada4a45d2a63
Alexander Wang <zxcvb22217@gmail.com>
2021-09-12 12:57:08 +0800

    feat(App): Customize scrollbar styling

--- src/core/App.styles.ts
+++ src/core/App.styles.ts
@@ -34,6 +34,21 @@ export const AppStyles = stylesheet({
     height: '100%',
     color: theme.colors.contentText,
     background: theme.colors.bgPrimary,
+    $nest: {
+      '& ::-webkit-scrollbar': {
+        width: '.5rem',
+        height: '.5rem',
+        backgroundColor: 'inherit',
+      },
+      '& ::-webkit-scrollbar-thumb': {
+        background: theme.colors.bgHover,
+        $nest: {
+          '&:active': {
+            background: theme.colors.bgActive,
+          },
+        },
+      },
+    },
   },
   'App--BlockMoving': {
     cursor: 'grabbing',
