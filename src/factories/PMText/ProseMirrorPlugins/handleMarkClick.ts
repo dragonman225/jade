356fa430c0d69b435d7c298dff03b26678527fb7
Alexander Wang <zxcvb22217@gmail.com>
2021-09-18 17:10:25 +0800

    fix(PMText): Prevent link click if moved

--- src/factories/PMText/ProseMirrorPlugins/handleMarkClick.ts
+++ src/factories/PMText/ProseMirrorPlugins/handleMarkClick.ts
@@ -1,6 +1,8 @@
 import { Mark } from 'prosemirror-model'
 import { Plugin } from 'prosemirror-state'
 
+import { distanceOf, getUnifiedClientCoords } from '../../../core/utils'
+
 export class MarkClickRule {
   markName: string
   handler: (attrs: Mark['attrs'], event: MouseEvent) => void
@@ -21,15 +23,41 @@ export function handleMarkClick({
 }): Plugin {
   return new Plugin({
     props: {
-      handleClick(view, pos, event) {
-        const { doc } = view.state
-        const node = doc.nodeAt(pos)
-        if (!node) return false
-        node.marks.forEach(mark => {
-          const matchRule = rules.find(rule => rule.markName === mark.type.name)
-          if (matchRule) matchRule.handler(mark.attrs, event)
-        })
-        return false
+      handleDOMEvents: {
+        mousedown(view, pointerDownEvent) {
+          /** Non primary buttons should not produce a click. */
+          if (pointerDownEvent.button !== 0) return false
+
+          function interpretClick(clickEvent: MouseEvent) {
+            window.removeEventListener('click', interpretClick)
+            const downPoint = getUnifiedClientCoords(pointerDownEvent)
+            const upPoint = getUnifiedClientCoords(clickEvent)
+            const moveThreshold = 3
+            /** If the pointer moved beyond the threshold, it's not a click. */
+            if (distanceOf(downPoint, upPoint) > moveThreshold) {
+              clickEvent.preventDefault()
+              return
+            }
+
+            /** If it's a click, we call the handlers. */
+            const posInfo = view.posAtCoords({
+              left: upPoint.x,
+              top: upPoint.y,
+            })
+            if (!posInfo) return
+            const node = view.state.doc.nodeAt(posInfo.pos)
+            if (!node) return
+            node.marks.forEach(mark => {
+              const matchRule = rules.find(
+                rule => rule.markName === mark.type.name
+              )
+              if (matchRule) matchRule.handler(mark.attrs, clickEvent)
+            })
+          }
+
+          window.addEventListener('click', interpretClick)
+          return false
+        },
       },
     },
   })
