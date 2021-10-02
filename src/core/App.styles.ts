a4ce9197a4a690f930fbebe1ae772b952b92cfd8
Alexander Wang <zxcvb22217@gmail.com>
2021-10-03 01:17:39 +0800

    refactor: Move global CSS to index.html

--- src/core/App.styles.ts
+++ src/core/App.styles.ts
@@ -1,32 +1,6 @@
-import { cssRaw, stylesheet } from 'typestyle'
+import { stylesheet } from 'typestyle'
 
 import theme from '../theme'
-
-cssRaw(`
-*::selection {
-  background: hsla(165deg, 70%, 46%, 0.27);
-}
-
-* {
-  box-sizing: border-box;
-  user-select: none;
-}
-
-html, body, #react-root {
-  margin: 0;
-  height: 100%;
-  overflow: hidden;
-}
-
-:root {
-  font-size: 18px;
-  font-family:
-    'Noto Sans', 'Noto Sans CJK TC',
-    -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
-    'Helvetica Neue', sans-serif;
-  line-height: 1.6;
-}`)
 
 export const AppStyles = stylesheet({
   App: {
