abf0fccf3442c400f8f223c0fd1c35e31270781e
Alexander Wang <zxcvb22217@gmail.com>
2021-10-15 02:42:10 +0800

    feat(App): Add transition for background
    * Provide smooth color transition when using CSS blocks.

--- src/core/App.styles.ts
+++ src/core/App.styles.ts
@@ -8,6 +8,7 @@ export const AppStyles = stylesheet({
     height: '100%',
     color: theme.colors.contentText,
     background: theme.colors.bgCanvas,
+    transition: `background 0.3s ${theme.easings.easeInOutCubic}`,
     $nest: {
       '& ::-webkit-scrollbar': {
         width: '.5rem',
