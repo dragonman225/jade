71248784034828c1848186be603a8a10402a4169
Alexander Wang <zxcvb22217@gmail.com>
2021-10-15 02:35:50 +0800

    refactor(factories/commonStyles): Use theme padding

--- src/factories/commonStyles.ts
+++ src/factories/commonStyles.ts
@@ -2,7 +2,7 @@ import theme from '../theme'
 
 export const emptyNavItem = {
   fontSize: '.8rem',
-  padding: '.5rem',
+  padding: theme.paddings.navComfort,
   maxHeight: '100%',
   color: theme.colors.uiGrey,
 }
