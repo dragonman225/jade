5ac4e219afce86836de2dbd6439dda64e9f0b5dc
Alexander Wang <zxcvb22217@gmail.com>
2021-11-27 00:25:22 +0800

    docs(core/utils/math): document parameters in ArrowDescriptor

--- src/core/utils/math.ts
+++ src/core/utils/math.ts
@@ -184,15 +184,21 @@ export function cartesianCoordsToPolarCoords(
 
 /** Information of an arrow. */
 type ArrowDescriptor = [
+  /** start point */
   sx: number,
   sy: number,
+  /** control point for start point */
   c1x: number,
   c1y: number,
+  /** control point for end point */
   c2x: number,
   c2y: number,
+  /** end point */
   ex: number,
   ey: number,
+  /** angle of end point */
   ae: number,
+  /** angle of start point */
   as: number
 ]
 
