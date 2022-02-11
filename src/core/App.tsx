f17ebe598d7f17ffda54403babeda80cde1b01b6
Alexander Wang <zxcvb22217@gmail.com>
2022-02-12 02:01:59 +0800

    perf(core/App): extract efficient rendering logic

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -9,6 +9,7 @@ import { Arrow } from './components/Arrow'
 import { ArrowList } from './components/ArrowList'
 import { BlockList } from './components/BlockList'
 import { CanvasInteractionDetector } from './components/CanvasInteractionDetector'
+import { EfficientBlockList } from './components/EfficientBlockList'
 import { PinnedPositioned } from './components/PinnedPositioned'
 import { NormalPositioned } from './components/NormalPositioned'
 import { ViewObject } from './components/ViewObject'
@@ -22,6 +23,7 @@ import theme from '../theme'
 import { createAppStateReducer, loadAppState } from './store/reducer'
 import {
   AppState,
+  BlockInstance,
   DatabaseInterface,
   FactoryRegistry,
   InteractionMode,
@@ -29,11 +31,7 @@ import {
   Rect,
 } from './interfaces'
 import { Action, Actions } from './store/actions'
-import {
-  boundingBoxOfBoxes,
-  growBox,
-  isBoxBoxIntersectingObjVer,
-} from './utils'
+import { boundingBoxOfBoxes, growBox } from './utils'
 import { blockRectManager } from './utils/blockRectManager'
 import { blockToBox, findBlock, isMovingBlocks } from './utils/block'
 import { SystemContext } from './store/systemContext'
@@ -49,55 +47,10 @@ const App = React.memo(function App() {
     dispatchAction({ type: Action.BlocksRendered })
   }, [dispatchAction])
 
-  const normalBlocks = useMemo(() => {
-    /** Virtualized rendering wastes CPU time when zooming. */
-    if (state.shouldAnimateCamera) {
-      return state.blocks.filter(b => b.posType === PositionType.Normal)
-    }
-
-    const windowWidth = window.innerWidth
-    const windowHeight = window.innerHeight
-    const overscanX = windowWidth * 0.25
-    const overscanY = windowHeight * 0.25
-    const visibleArea = {
-      x: state.camera.focus.x - overscanX,
-      y: state.camera.focus.y - overscanY,
-      w: windowWidth / state.camera.scale + 2 * overscanX,
-      h: windowHeight / state.camera.scale + 2 * overscanY,
-    }
-    const blockSizes = state.blocks.map(b =>
-      blockRectManager.getMeasuredSize(b.id)
-    )
-
-    return state.blocks.filter((b, index) => {
-      const blockSize = blockSizes[index]
-
-      return (
-        b.posType === PositionType.Normal &&
-        /**
-         * If a Block haven't reported its rect, render it anyway so it can
-         * report. Usually this happens when opening a Canvas and
-         * double-clicking to create a new Block + Concept.
-         */
-        (!blockSize ||
-          !settings.shouldEnableEfficientRendering ||
-          isBoxBoxIntersectingObjVer(
-            {
-              ...b.pos,
-              w: typeof b.size.w === 'number' ? b.size.w : blockSize.width,
-              h: typeof b.size.h === 'number' ? b.size.h : blockSize.height,
-            },
-            visibleArea
-          ))
-      )
-    })
-  }, [
-    state.blocks,
-    state.camera,
-    settings.shouldEnableEfficientRendering,
-    state.shouldAnimateCamera,
-  ])
-
+  const normalBlockSelector = useCallback(
+    (b: BlockInstance) => b.posType === PositionType.Normal,
+    []
+  )
   const pinnedBlocks = state.blocks.filter(b => b.posType > PositionType.Normal)
 
   const contextMenuDesiredRect: Rect = useMemo(
@@ -127,13 +80,16 @@ const App = React.memo(function App() {
           selectionBox={state.selectionBox}
           shouldAnimate={state.shouldAnimateCamera}>
           {state.blocksRendered && <ArrowList />}
-          <BlockList
+          <EfficientBlockList
             /**
              * Use key to force unmount and re-mount on canvas switch, to
              * make sure `onRender` get called.
              */
             key={state.viewingConcept.id}
-            blocks={normalBlocks}
+            blocks={state.blocks}
+            camera={state.camera}
+            shouldRenderOnlyVisible={settings.shouldEnableEfficientRendering}
+            selectBlock={normalBlockSelector}
             onRender={notifyBlocksRendered}
           />
           {state.blocksRendered &&
---
+++ src/core/components/EfficientBlockList.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import { useMemo } from 'react'
+
+import { BlockList } from './BlockList'
+import { BlockInstance, Camera } from '../interfaces'
+import { isBoxBoxIntersectingObjVer } from '../utils'
+import { blockRectManager } from '../utils/blockRectManager'
+
+interface Props {
+  blocks: BlockInstance[]
+  camera: Camera
+  shouldRenderOnlyVisible: boolean
+  /**
+   * Return `true` to let the block go into efficient rendering pipeline.
+   *
+   * Should be stable, or the list will re-render even when other props
+   * don't change.
+   */
+  selectBlock: (block: BlockInstance) => boolean
+  onRender?: () => void
+}
+
+export function EfficientBlockList({
+  blocks,
+  camera,
+  shouldRenderOnlyVisible,
+  selectBlock,
+  onRender,
+}: Props): JSX.Element {
+  const visibleBlocks = useMemo(() => {
+    if (!shouldRenderOnlyVisible) {
+      return blocks.filter(selectBlock)
+    }
+
+    const windowWidth = window.innerWidth
+    const windowHeight = window.innerHeight
+    const overscanX = windowWidth * 0.25
+    const overscanY = windowHeight * 0.25
+    const visibleArea = {
+      x: camera.focus.x - overscanX,
+      y: camera.focus.y - overscanY,
+      w: windowWidth / camera.scale + 2 * overscanX,
+      h: windowHeight / camera.scale + 2 * overscanY,
+    }
+    const blockSizes = blocks.map(b => blockRectManager.getMeasuredSize(b.id))
+
+    return blocks.filter((b, index) => {
+      const blockSize = blockSizes[index]
+
+      return (
+        selectBlock(b) &&
+        /**
+         * If a Block haven't reported its rect, render it anyway so it can
+         * report. Usually this happens when opening a Canvas and
+         * double-clicking to create a new Block + Concept.
+         */
+        (!blockSize ||
+          isBoxBoxIntersectingObjVer(
+            {
+              x: b.pos.x,
+              y: b.pos.y,
+              w: typeof b.size.w === 'number' ? b.size.w : blockSize.width,
+              h: typeof b.size.h === 'number' ? b.size.h : blockSize.height,
+            },
+            visibleArea
+          ))
+      )
+    })
+  }, [blocks, camera, shouldRenderOnlyVisible, selectBlock])
+
+  return <BlockList blocks={visibleBlocks} onRender={onRender} />
+}
